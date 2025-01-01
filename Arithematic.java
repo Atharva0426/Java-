a8b59cbbee6485666040e645926749790c761b8e
Aharva Karale <atharvakarale2604@gmail.com>
2025-01-01 23:31:09 +0530

    Add files via upload

---
+++ Arithematic.java
@@ -0,0 +1,11 @@
+public class Arithematic {
+    public static void main(String[] args) {
+        int a=2,b=3;
+        System.out.println("Addition of no:" +(a+b));
+        System.out.println("Substraction of no:"+(a-b));
+        System.out.println("Multipliction of no:"+(a*b));
+        System.out.println("Division of no:"+(a/b));
+        System.out.println("Modulo of no:"+(a%b));  //print the remainder
+    }
+    
+}
